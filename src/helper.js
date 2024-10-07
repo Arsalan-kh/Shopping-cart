1a927062b331d7ba64c838c9204cde2bf6048b1e
Arsalan <arsalankhan871483@gmail.com>
2024-10-07 13:45:38 +0500

    fix ui

--- src/helper.js
+++ src/helper.js
@@ -40,101 +40,62 @@ export const accordionData = [
     {
         heading: "Floorplans",
         items: [
-            { id: 4, name: "Melo - Large Floor Plan ", price: 319, isChecked: false },
-            { id: 5, name: " Melo - Medium Floor Plan ", price: 242, isChecked: false },
-            { id: 6, name: "Melo - Small Floor Plan", price: 193, isChecked: false },
-            { id: 7, name: "Melo - Redraw Large Floorplan", price: 198, isChecked: false },
-            { id: 8, name: "Redraw Medium Floorplan", price: 143, isChecked: false },
-            { id: 9, name: "Redraw Small Floorplan", price: 99, isChecked: false },
+            { id: 12, name: "Melo - Large Floor Plan ", price: 319, isChecked: false },
+            { id: 13, name: " Melo - Medium Floor Plan ", price: 242, isChecked: false },
+            { id: 14, name: "Melo - Small Floor Plan", price: 193, isChecked: false },
+            { id: 15, name: "Melo - Redraw Large Floorplan", price: 198, isChecked: false },
+            { id: 16, name: "Redraw Medium Floorplan", price: 143, isChecked: false },
+            { id: 17, name: "Redraw Small Floorplan", price: 99, isChecked: false },
 
         ],
     },
     {
         heading: 'Video',
         items: [
-            { id: 1, name: 'Melo - Property Video', price: 1150, isChecked: false },
-            { id: 2, name: 'Melo - Storytelling Videos', price: 2200, isChecked: false },
+            { id: 18, name: 'Melo - Property Video', price: 1150, isChecked: false },
+            { id: 19, name: 'Melo - Storytelling Videos', price: 2200, isChecked: false },
         ],
     },
     {
         heading: 'Copy & Social Media',
         items: [
-            { id: 3, name: 'Melo - Property Copywriting', price: 140, isChecked: false },
-            { id: 4, name: 'Melo - Social Media Advertising', price: 300, isChecked: false },
+            { id: 20, name: 'Melo - Property Copywriting', price: 140, isChecked: false },
+            { id: 21, name: 'Melo - Social Media Advertising', price: 300, isChecked: false },
         ],
     },
     {
         heading: 'Signboards',
         items: [
-            { id: 5, name: 'Melo - 8x4 Satin Laminated Edge Wrap Signboard', price: 375, isChecked: false },
-            { id: 6, name: 'Melo - 8x6 Satin Laminated Edge Wrap Signboard', price: 780, isChecked: false },
-            { id: 7, name: 'Melo - 8x6 Solar Illuminated Signboard', price: 1112, isChecked: false },
+            { id: 22, name: 'Melo - 8x4 Satin Laminated Edge Wrap Signboard', price: 375, isChecked: false },
+            { id: 23, name: 'Melo - 8x6 Satin Laminated Edge Wrap Signboard', price: 780, isChecked: false },
+            { id: 24, name: 'Melo - 8x6 Solar Illuminated Signboard', price: 1112, isChecked: false },
         ],
     },
     {
         heading: 'Internet Portals',
         items: [
-            { id: 8, name: 'Realestate.com.au', price: 0, isChecked: false },
-            { id: 9, name: 'Domain.com.au', price: 0, isChecked: false }, // Editable API item
-            { id: 10, name: 'Campaign Fee', price: 40, isChecked: false },
+            { id: 25, name: 'Realestate.com.au', price: 0, isChecked: false },
+            { id: 26, name: 'Domain.com.au', price: 0, isChecked: false }, // Editable API item
+            { id: 27, name: 'Campaign Fee', price: 40, isChecked: false },
         ],
     },
     {
         heading: 'Auctioneer',
         items: [
-            { id: 11, name: 'Narz Sayed - Auctioneer', price: 550, isChecked: false },
-            { id: 12, name: 'Andrew Cooley - Auctioneer', price: 995, isChecked: false },
+            { id: 28, name: 'Narz Sayed - Auctioneer', price: 550, isChecked: false },
+            { id: 29, name: 'Andrew Cooley - Auctioneer', price: 995, isChecked: false },
         ],
     },
     {
         heading: 'I.M Group',
         items: [
-            { id: 13, name: 'The Merjan Group Package', price: 1500, isChecked: false },
-            { id: 14, name: 'The Merjan Group Package with video', price: 2274, isChecked: false },
-            { id: 15, name: 'I.M Group Pty Ltd - realestate.com.au (Portal Export)', price: 2200, isChecked: false },
-            { id: 16, name: 'I.M Group Pty Ltd - domain.com.au (API) - Existing Build', price: 250, isChecked: false },
-            { id: 17, name: 'The Merjan Group Auctioneer', price: 795, isChecked: false },
-            { id: 18, name: 'The Merjan Group DL Package', price: 532, isChecked: false },
+            { id: 30, name: 'The Merjan Group Package', price: 1500, isChecked: false },
+            { id: 31, name: 'The Merjan Group Package with video', price: 2274, isChecked: false },
+            { id: 32, name: 'I.M Group Pty Ltd - realestate.com.au (Portal Export)', price: 2200, isChecked: false },
+            { id: 33, name: 'I.M Group Pty Ltd - domain.com.au (API) - Existing Build', price: 250, isChecked: false },
+            { id: 34, name: 'The Merjan Group Auctioneer', price: 795, isChecked: false },
+            { id: 35, name: 'The Merjan Group DL Package', price: 532, isChecked: false },
         ],
     },
 ];
 
-
-// Video
-
-// Melo - Property Video $1150
-// Melo - Storytelling Videos $2200
-
-// Copy & social media
-
-// Melo - Property Copywriting $140
-// Melo - Social Media Advertising $300
-
-// Signboards
-// Melo - 8x4 Satin Laminated Edge Wrap Signboard $375
-// Melo - 8x6 Satin Laminated Edge Wrap Signboard $780
-// Melo - 8x6 Solar Illuminated Signboard $1112
-
-// Internet Portals
-
-// Realestate.com.au - $0
-// Domain.com.au - $0 (this for api so we can edit)
-// Campaign Fee $40
-
-// Auctioneer
-
-// Narz Sayed - Auctioneer $550
-// Andrew Cooley Auctioneer  $995
-
-// I.M Group
-
-// The Merjan Group Package $1500
-// The Merjan Group Package with video $2274
-// I.M Group Pty Ltd *realestate.com.au (Portal Export) $2200
-// I.M Group Pty Ltd domain.com.au (API) - Existing Build $250
-// The Merjan Group Auctioneer - $795
-// The Merjan Group DL Package - $532
-
-
-
-// Agent Contribution $0 (but this needs to minus the total if entered - edit button)
